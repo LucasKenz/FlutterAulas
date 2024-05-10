ffc8e9462b0d668181d0b3f03763c258e8f037ed
LucasKenz <lkmtada@gmail.com>
2024-05-10 14:49:35 -0300

    flutter encontro 7 constraint

---
+++ aula 7/flutter_application_encontro_7_constraints/lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_encontro_7_constraints/pages/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ aula 7/flutter_application_encontro_7_constraints/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_encontro_7_constraints/app.dart';
+
+void main() {
+  runApp(const App());
+}
